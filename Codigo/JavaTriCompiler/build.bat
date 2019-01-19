c6c0be55fd16e3058d09eb2e3d867c2696c5a19b
GabrielRA <gabrira09@gmail.com>
2019-01-18 18:57:54 -0600

    Compilador Java para Pruebas

---
+++ Codigo/JavaTriCompiler/build.bat
@@ -0,0 +1,10 @@
+javac -cp C:\JavaTriCompiler;C:\JavaTriCompiler\Triangle Triangle/AbstractSyntaxTrees/Visitor.java
+javac -cp C:\JavaTriCompiler;C:\JavaTriCompiler\Triangle Triangle/SyntacticAnalyzer/Token.java
+javac -cp C:\JavaTriCompiler;C:\JavaTriCompiler\Triangle Triangle/SyntacticAnalyzer/Parser.java
+javac -cp C:\JavaTriCompiler;C:\JavaTriCompiler\Triangle Triangle/TreeDrawer/LayoutVisitor.java
+javac -cp C:\JavaTriCompiler;C:\JavaTriCompiler\Triangle Triangle/CodeGenerator/Encoder.java
+javac -cp C:\JavaTriCompiler;C:\JavaTriCompiler\Triangle Triangle/ContextualAnalyzer/Checker.java
+javac -cp C:\JavaTriCompiler;C:\JavaTriCompiler\Triangle Triangle/Printers/*.java
+javac -cp C:\JavaTriCompiler;C:\JavaTriCompiler\Triangle Triangle/Compiler.java
+
+java Triangle.Compiler caso1.tri
